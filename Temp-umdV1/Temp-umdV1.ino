579a1f901e3ead89c671446c9f38aa19b2e0e956
TheRealOpTezaur <therealtezaur@gmail.com>
2024-12-13 02:05:03 +0200

    Add files via upload

---
+++ Temp-umdV1/Temp-umdV1.ino
@@ -0,0 +1,43 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-temperature-humidity-sensor
+ */
+
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin(); // initialize the sensor
+}
+
+void loop() {
+  // wait a few seconds between measurements.
+  delay(2000);
+
+  // read humidity
+  float humi  = dht.readHumidity();
+  // read temperature as Celsius
+  float tempC = dht.readTemperature();
+
+  // check if any reads failed
+  if (isnan(humi) || isnan(tempC)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+
+    Serial.print("  |  "); 
+
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.println("°C");
+  }
+}
